2ec6cfbc187bc30a8ae71e95496b81d378e03ab4
Thomas Scholtes <thomas-scholtes@gmx.de>
2014-04-04 20:16:42 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -1,33 +1,32 @@
 from setuptools import setup
 
-with open('README.md') as desc:
-    long_description = desc.read()
-setup(name='beets-check',
-      version='0.9.0-beta.2',
-      description='beets plugin verifying file integrity with checksums',
-      long_description=long_description,
-      author='Thomas Scholtes',
-      author_email='thomas-scholtes@gmx.de',
-      url='http://www.github.com/geigerzaehler/beets-check',
-      license='MIT',
-      platforms='ALL',
+setup(
+    name='beets-check',
+    version='0.9.0-beta.3',
+    description='beets plugin verifying file integrity with checksums',
+    long_description=open('README.md').read(),
+    author='Thomas Scholtes',
+    author_email='thomas-scholtes@gmx.de',
+    url='http://www.github.com/geigerzaehler/beets-check',
+    license='MIT',
+    platforms='ALL',
 
-      test_suite='test',
+    test_suite='test',
 
-      packages=['beetsplug'],
+    packages=['beetsplug'],
 
-      install_requires=[
-          'beets>=1.3.4',
-      ],
+    install_requires=[
+        'beets>=1.3.4',
+    ],
 
-      classifiers=[
-          'Topic :: Multimedia :: Sound/Audio',
-          'Topic :: Multimedia :: Sound/Audio :: Players :: MP3',
-          'License :: OSI Approved :: MIT License',
-          'Environment :: Console',
-          'Environment :: Web Environment',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-      ],
+    classifiers=[
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Sound/Audio :: Players :: MP3',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
